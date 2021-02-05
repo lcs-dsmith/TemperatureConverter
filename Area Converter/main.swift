c9fb105510979327e2e06d042e6c2fbd7806b4d3
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-04 20:37:19 -0500

    Convert length functions are both done as well as collecting the input

--- Area Converter/main.swift
+++ Area Converter/main.swift
@@ -11,5 +11,24 @@ print("Hello, World!")
 
 // Different measurment types, MM, CM, M, KM, IN, FT, YRD, MI
 
+//MARK: Input
 
+print("""
+    This program works with the following values:
+milimetres (mm)
+centimetres (cm)
+metres (m)
+kilometres (km)
+inches (in)
+feet (ft)
+yards (yrd)
+miles (mi)
+    """)
+let startingSystem = String.collectInput(withPrompt: "What is your starting system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
+
+let startingSystem = String.collectInput(withPrompt: "What is your destination system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
+
+let valueToComvert = Int.collectInput(withPrompt: "What is the value you want to convert? ", minimum: 0, maximum: nil)
+
+//MRK: process
 
