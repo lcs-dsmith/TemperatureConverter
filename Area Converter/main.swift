2c887d9705c45677776dcf537652f60c55b8efba
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-04 20:43:57 -0500

    added loop

--- Area Converter/main.swift
+++ Area Converter/main.swift
@@ -7,13 +7,12 @@
 
 import Foundation
 
-print("Hello, World!")
 
 // Different measurment types, MM, CM, M, KM, IN, FT, YRD, MI
 
 //MARK: Input
-
-print("""
+while true {
+    print("""
     This program works with the following values:
 milimetres (mm)
 centimetres (cm)
@@ -24,16 +23,25 @@ feet (ft)
 yards (yrd)
 miles (mi)
     """)
-let startingSystem = String.collectInput(withPrompt: "What is your starting system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
-
-let targetSystem = String.collectInput(withPrompt: "What is your destination system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
-
-let valueToComvert = Double.collectInput(withPrompt: "What is the value you want to convert? ", minimum: 0, maximum: nil)
-
-//MRK: process
-
-let metresValue = convertToMeters(from: startingSystem, of: valueToComvert)
-let convertedValue = convertToTargetSystem(from: targetSystem, of: metresValue)
-
-//MARK: output
-print("You conversion is \(convertedValue) \(targetSystem)")
+    let startingSystem = String.collectInput(withPrompt: "What is your starting system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
+    
+    let targetSystem = String.collectInput(withPrompt: "What is your destination system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
+    
+    let valueToComvert = Double.collectInput(withPrompt: "What is the value you want to convert? ", minimum: 0, maximum: nil)
+    
+    //MRK: process
+    
+    let metresValue = convertToMeters(from: startingSystem, of: valueToComvert)
+    let convertedValue = convertToTargetSystem(from: targetSystem, of: metresValue)
+    
+    //MARK: output
+    print("You conversion is \(convertedValue) \(targetSystem)")
+    
+    //MARK: loop
+    let continueYesOrNO = String.collectInput(withPrompt: "Would you like to calculate again yes or no? ", acceptableValues: ["yes", "no"])
+    if continueYesOrNO == "no" {
+        exit(0)
+    } else {
+        continue
+    }
+}
