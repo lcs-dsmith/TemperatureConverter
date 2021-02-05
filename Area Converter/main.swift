42f979a42d989be34f506dd831f647091a62f530
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-04 20:41:53 -0500

    process and output are done

--- Area Converter/main.swift
+++ Area Converter/main.swift
@@ -26,9 +26,14 @@ miles (mi)
     """)
 let startingSystem = String.collectInput(withPrompt: "What is your starting system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
 
-let startingSystem = String.collectInput(withPrompt: "What is your destination system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
+let targetSystem = String.collectInput(withPrompt: "What is your destination system? ", acceptableValues: ["mm", "cm", "m", "km", "in", "ft", "yrd", "mi"])
 
-let valueToComvert = Int.collectInput(withPrompt: "What is the value you want to convert? ", minimum: 0, maximum: nil)
+let valueToComvert = Double.collectInput(withPrompt: "What is the value you want to convert? ", minimum: 0, maximum: nil)
 
 //MRK: process
 
+let metresValue = convertToMeters(from: startingSystem, of: valueToComvert)
+let convertedValue = convertToTargetSystem(from: targetSystem, of: metresValue)
+
+//MARK: output
+print("You conversion is \(convertedValue) \(targetSystem)")
